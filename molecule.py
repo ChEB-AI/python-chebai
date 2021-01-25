2a7a24c79a07c242a1ed1ab03ed7c18b7ee4c4be
MGlauer <martinglauer89@gmail.com>
2021-01-25 08:44:20 +0100

    Reset valence

--- molecule.py
+++ molecule.py
@@ -134,10 +134,10 @@ class Molecule:
                                                         'Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In',
                                                         'Mn',
                                                         'Zr', 'Cr', 'Pt', 'Hg', 'Pb', 'Unknown']) +
-                        Molecule.one_of_k_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5, 6, 7]) +
+                        Molecule.one_of_k_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5]) +
                         Molecule.one_of_k_encoding_unk(atom.GetTotalNumHs(), [0, 1, 2, 3, 4]) +
                         Molecule.one_of_k_encoding_unk(atom.GetImplicitValence(),
-                                                       [0, 1, 2, 3, 4, 5, 6, 7]) + [atom.GetIsAromatic()])
+                                                       [0, 1, 2, 3, 4, 5]) + [atom.GetIsAromatic()])
 
     @staticmethod
     def atom_features_of_contract_rings(degree):
