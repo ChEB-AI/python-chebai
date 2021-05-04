71834d9f85cc469e57fd79395edc914699cad707
MGlauer <martinglauer89@gmail.com>
2021-05-04 12:12:33 +0200

    Use higher learning rate

--- lstm.py
+++ lstm.py
@@ -52,7 +52,7 @@ class ChemLSTM(pl.LightningModule):
             return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
+        optimizer = torch.optim.Adam(self.parameters())
         return optimizer
 
     def forward(self, x):
