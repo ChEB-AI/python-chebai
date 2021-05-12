a4ce3d1e2343ea38d3c865e7adab09827b6a9263
MGlauer <martinglauer89@gmail.com>
2021-05-12 09:54:59 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ChEBI-learn',
+    version='0.0.0',
+    packages=['chem', 'chem.models'],
+    url='',
+    license='',
+    author='MGlauer',
+    author_email='martin.glauer@ovgu.de',
+    description=''
+)
