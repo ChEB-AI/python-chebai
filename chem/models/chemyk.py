1f3846126031ffc79e702cd484870dfd8109df95
MGlauer <martinglauer89@gmail.com>
2021-08-17 16:21:21 +0200

    Add non-linearity to chemyk

--- chem/models/chemyk.py
+++ chem/models/chemyk.py
@@ -7,7 +7,7 @@ from torch import nn
 
 from torch.nn.functional import pad
 import torch
-from torch import functional as F
+from torch.nn import functional as F
 import logging
 from chem.models.base import JCIBaseNet
 
@@ -27,6 +27,7 @@ class ChemYK(JCIBaseNet):
         self.a_r = nn.Linear(d_internal, 1)
         self.w_l = nn.Linear(d_internal, d_internal)
         self.w_r = nn.Linear(d_internal, d_internal)
+        self.norm = nn.LayerNorm(d_internal)
         self.output = nn.Sequential(nn.Linear(in_d, in_d), nn.ReLU(), nn.Dropout(0.2), nn.Linear(in_d, num_classes))
 
     def forward(self, data, *args, **kwargs):
@@ -39,12 +40,12 @@ class ChemYK(JCIBaseNet):
             r = torch.stack(tuple(h[i][:,(width-i):] for i in range(0, width))).flip(0)
             m = self.merge(l,r)
             h.append(m)
-        return self.output(m[:,0]).squeeze(1)
+        return self.output(m).squeeze(1)
 
     def merge(self, l, r):
         x = torch.stack([self.a_l(l), self.a_r(r)])
         beta = torch.softmax(x, 0)
-        return self.attention(torch.sum(beta*torch.stack([self.w_l(l), self.w_r(r)]), dim=0))
+        return F.leaky_relu(self.attention(torch.sum(beta*torch.stack([self.w_l(l), self.w_r(r)]), dim=0)))
 
     def attention(self, parts):
         at = torch.softmax(self.s(parts), 1)
