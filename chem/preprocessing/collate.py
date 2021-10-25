3ba090386c59c3ec26d84b708992e7ed2e28d17f
MGlauer <martinglauer89@gmail.com>
2021-10-25 18:11:00 +0200

    Implement collater classes

---
+++ chem/preprocessing/collate.py
@@ -0,0 +1,27 @@
+from chem.preprocessing.structures import XYData
+import torch
+from torch.nn.utils.rnn import pad_sequence
+
+
+class Collater:
+    def __init__(self, **kwargs):
+        pass
+
+    def __call__(self, data):
+        raise NotImplementedError
+
+
+class DefaultCollater(Collater):
+    def __call__(self, data):
+        x, y = zip(*data)
+        return XYData(x, y)
+
+
+class RaggedCollater(Collater):
+    def __call__(self, data):
+        x, y = zip(*data)
+        return XYData(pad_sequence([torch.tensor(a) for a in x],
+                                   batch_first=True),
+                      pad_sequence([torch.tensor(a) for a in y],
+                                   batch_first=True),
+                      additional_fields=dict(lens=list(map(len, x))))
