a2a02191ab6a1b6ac9a21ad7a6a05ab4302a1808
MGlauer <martinglauer89@gmail.com>
2021-10-26 13:29:49 +0200

    Catch missing experiment key

--- chem/run.py
+++ chem/run.py
@@ -6,7 +6,13 @@ from chem import experiments
 def main():
     batch_size = int(sys.argv[2])
     experiment_id = sys.argv[1]
-    ex = experiments.EXPERIMENTS[experiment_id](batch_size, *sys.argv[3:])
+    try:
+        ex = experiments.EXPERIMENTS[experiment_id](batch_size, *sys.argv[3:])
+    except KeyError:
+        raise Exception(
+            "Experiment ID not found. The following are available:"
+            + ", ".join(experiments.EXPERIMENTS.keys())
+        )
     ex.execute()
 
 
