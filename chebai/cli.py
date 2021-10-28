c64b93f7bf63ef315370b83fe7541668505417dc
MGlauer <martinglauer89@gmail.com>
2021-10-28 13:38:42 +0200

    Implement prediction framework

--- chebai/cli.py
+++ chebai/cli.py
@@ -15,13 +15,30 @@ def cli():
 def train(experiment, batch_size, args):
     """Run experiment identified by EXPERIMENT in batches of size BATCH_SIZE."""
     try:
-        ex = experiments.EXPERIMENTS[experiment](batch_size, *args)
+        ex = experiments.EXPERIMENTS[experiment](*args)
     except KeyError:
         raise Exception(
             "Experiment ID not found. The following are available:"
             + ", ".join(experiments.EXPERIMENTS.keys())
         )
-    ex.execute()
+    ex.train(batch_size)
+
+
+@click.command()
+@click.argument("experiment")
+@click.argument("ckpt_path")
+@click.argument("data_path")
+def predict(experiment, ckpt_path, data_path):
+    """Run experiment identified by EXPERIMENT in batches of size BATCH_SIZE."""
+    try:
+        ex = experiments.EXPERIMENTS[experiment]()
+    except KeyError:
+        raise Exception(
+            "Experiment ID not found. The following are available:"
+            + ", ".join(experiments.EXPERIMENTS.keys())
+        )
+    ex.predict(ckpt_path, data_path)
 
 
 cli.add_command(train)
+cli.add_command(predict)
