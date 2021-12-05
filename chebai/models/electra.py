93c76ec4d0d358830e357ade0cab6f34d33bcbb1
MGlauer <martinglauer89@gmail.com>
2021-12-05 13:45:39 +0100

    Improve electra replacement logic

--- chebai/models/electra.py
+++ chebai/models/electra.py
@@ -1,5 +1,6 @@
 from tempfile import TemporaryDirectory
 import logging
+import random
 
 from torch import nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
@@ -21,12 +22,37 @@ class ElectraPre(JCIBaseNet):
     NAME = "ElectraPre"
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super().__init__(**kwargs, p=0.2)
+        self._p = 0.2
         self.config = ElectraConfig(**kwargs["config"])
         self.electra = ElectraForPreTraining(self.config)
 
+    def _get_data_and_labels(self, batch, batch_idx):
+        batch_labels = []
+        batch_data = []
+        for row in batch.x:
+            vocabular = set(row)
+            labels = []
+            data = []
+            for t in list(row):
+                l = 0
+                if not all(x == t for x in vocabular) and random.random() < self._p:
+                    l = 1
+                    t0 = t
+                    while t0 == t:
+                        # this takes the list of all tokens by desing
+                        # it ensures that the probability distribution is roughly
+                        # the same as in the original string.
+                        t0 = random.choice(row)
+                    t = t0
+                data.append(t)
+                labels.append(l)
+            batch_data.append(data)
+            batch_labels.append(labels)
+        return torch.tensor(batch_data), torch.tensor(batch_labels)
+
     def forward(self, data):
-        x = data.x
+        x = data
         x = self.electra(x)
         return x.logits
 
