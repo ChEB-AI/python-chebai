a242c417db736ae418343aac6e101516a56b1928
MGlauer <martinglauer89@gmail.com>
2021-12-09 18:18:02 +0100

    Use proper `and` for tensors

--- chebai/models/electra.py
+++ chebai/models/electra.py
@@ -35,7 +35,7 @@ class ElectraPre(JCIBaseNet):
         equals = torch.eq(batch.x, subs).int()
 
         # exclude those indices where the replacement yields the same token
-        labels = ((labels_rnd < self._p) and (~equals)).int()
+        labels = torch.logical_and((labels_rnd < self._p), ~equals).int()
         features = (batch.x * (1 - labels)) + (subs * labels)
 
         return features, labels
