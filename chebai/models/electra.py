6b0624ad237fb395b5a96459f228af219c8ac5b8
MGlauer <martinglauer89@gmail.com>
2021-12-09 18:04:57 +0100

    Use newer Electra variant

--- chebai/models/electra.py
+++ chebai/models/electra.py
@@ -7,7 +7,7 @@ from transformers import (
     ElectraConfig,
     ElectraForMultipleChoice,
     ElectraForPreTraining,
-    ElectraModel,
+    ElectraForSequenceClassification,
     PretrainedConfig,
 )
 import torch
@@ -35,29 +35,23 @@ class Electra(JCIBaseNet):
     NAME = "Electra"
 
     def __init__(self, **kwargs):
+        # Remove this property in order to prevent it from being stored as a
+        # hyper parameter
+        pretrained_checkpoint = kwargs.pop("pretrained_checkpoint") or None
         super().__init__(**kwargs)
         self.config = ElectraConfig(**kwargs["config"], output_attentions=True)
 
-        if "pretrained_checkpoint" in kwargs:
+        if pretrained_checkpoint:
             elpre = ElectraPre.load_from_checkpoint(kwargs["pretrained_checkpoint"])
             with TemporaryDirectory() as td:
                 elpre.electra.save_pretrained(td)
-                self.electra = ElectraModel.from_pretrained(td, config=self.config)
+                self.electra = ElectraForSequenceClassification.from_pretrained(
+                    td, config=self.config
+                )
                 in_d = elpre.config.hidden_size
         else:
-            self.electra = ElectraModel(config=self.config)
+            self.electra = ElectraForSequenceClassification(config=self.config)
             in_d = self.config.hidden_size
-
-        self.output = nn.Sequential(
-            nn.Linear(in_d, in_d),
-            nn.ReLU(),
-            nn.Linear(in_d, in_d),
-            nn.ReLU(),
-            nn.Linear(in_d, in_d),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(in_d, 500),
-        )
 
     def forward(self, data):
         electra = self.electra(data.x)
