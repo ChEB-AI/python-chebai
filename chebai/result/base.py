a45ec597caa7675a16dc614f5323483d385b48d0
MGlauer <martinglauer89@gmail.com>
2022-01-26 12:10:36 +0100

    Make prediction interface more robust

--- chebai/result/base.py
+++ chebai/result/base.py
@@ -1,16 +1,17 @@
+from typing import Iterable
 import abc
+import multiprocessing as mp
+
+import torch
+import tqdm
+
 from chebai.models.base import JCIBaseNet
 from chebai.preprocessing.reader import DataReader
-import tqdm
-from typing import Iterable
-import torch
-import multiprocessing as mp
 
 PROCESSORS = dict()
 
 
 class ResultProcessor(abc.ABC):
-
     @classmethod
     def _identifier(cls) -> str:
         raise NotImplementedError
@@ -22,15 +23,21 @@ class ResultProcessor(abc.ABC):
         pass
 
     def __init_subclass__(cls, **kwargs):
-        assert cls._identifier() not in PROCESSORS, f"ResultProcessor {cls.__name__} does not have a unique identifier"
+        assert (
+            cls._identifier() not in PROCESSORS
+        ), f"ResultProcessor {cls.__name__} does not have a unique identifier"
         PROCESSORS[cls._identifier()] = cls
 
-    def process_prediction(self, proc_id, raw_features, raw_labels, features, labels, pred):
+    def process_prediction(
+        self, proc_id, raw_features, raw_labels, features, labels, pred
+    ):
         raise NotImplementedError
 
 
 class ResultFactory(abc.ABC):
-    def __init__(self, model: JCIBaseNet, dataset, processors: Iterable[ResultProcessor]):
+    def __init__(
+        self, model: JCIBaseNet, dataset, processors: Iterable[ResultProcessor]
+    ):
         self._model = model
         self._reader = dataset.reader
         self.dataset = dataset
@@ -41,8 +48,16 @@ class ResultFactory(abc.ABC):
 
     def _generate_predictions(self, data_path):
         self._model.eval()
-        for raw_features, raw_labels, features, labels in map(lambda x: (*self._reader._read_components(self._process_row(x)), *self._reader.to_data(self._process_row(x))), tqdm.tqdm(self.dataset._load_tuples(data_path))):
-            yield raw_features, raw_labels, features, labels, self._model(torch.tensor(features).unsqueeze(0))
+        for x in tqdm.tqdm(self.dataset._load_tuples(data_path)):
+            processed_row = self._process_row(x)
+            try:
+                features, labels = self._reader.to_data(processed_row)
+            except:
+                print("Error processing the following row:", x)
+            else:
+                yield *self._reader._read_components(
+                    processed_row
+                ), features, labels, self._model(torch.tensor(features).unsqueeze(0))
 
     def call_procs(self, args):
         proc_id, proc_args = args
@@ -54,7 +69,9 @@ class ResultFactory(abc.ABC):
             proc.start()
         try:
             with mp.Pool() as pool:
-                res = map(self.call_procs, enumerate(self._generate_predictions(data_path)))
+                res = map(
+                    self.call_procs, enumerate(self._generate_predictions(data_path))
+                )
             for r in res:
                 pass
 
@@ -63,4 +80,3 @@ class ResultFactory(abc.ABC):
         finally:
             for proc in self._processors:
                 proc.close()
-
