8aa86fda0c3deb4a498da545bccdfcf9b020640c
MGlauer <martinglauer89@gmail.com>
2023-03-08 18:37:22 +0100

    Remove GNN experiments

--- chebai/experiments.py
+++ chebai/experiments.py
@@ -319,37 +319,3 @@ class ElectraBPEOnJCIExt(_ElectraExperiment):
                 data_path=os.path.join(MODULE_PATH, "preprocessing/bin/BPE_SWJ")
             ),
         )
-
-
-class GATOnSWJ(Experiment):
-    MODEL = graph.JCIGraphAttentionNet
-
-    @classmethod
-    def identifier(cls) -> str:
-        return "GAT+JCIExt"
-
-    def model_kwargs(self, *args) -> Dict:
-        return dict(
-            in_length=50,
-            hidden_length=100,
-        )
-
-    def build_dataset(self, batch_size) -> datasets.XYBaseDataModule:
-        return datasets.JCIGraphData(batch_size)
-
-
-class GATOnTox21(Experiment):
-    MODEL = graph.JCIGraphAttentionNet
-
-    @classmethod
-    def identifier(cls) -> str:
-        return "GAT+Tox21"
-
-    def model_kwargs(self, *args) -> Dict:
-        return dict(
-            in_length=50,
-            hidden_length=100,
-        )
-
-    def build_dataset(self, batch_size) -> datasets.XYBaseDataModule:
-        return datasets.Tox21Graph(batch_size)
