503aed556d7cdab6e75f6993ed7d7781b2226421
MGlauer <martinglauer89@gmail.com>
2023-07-20 20:30:43 +0200

    Allow flexible loss definition

--- chebai/__init__.py
+++ chebai/__init__.py
@@ -1,3 +1,10 @@
 import os
+import torch
 
 MODULE_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+class CustomTensor(torch.Tensor):
+
+    def __new__(cls, data):
+        return torch.tensor(data)
