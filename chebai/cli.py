f29b1c1ba5ccba90c69a66b797231ca8244c0e13
MGlauer <martinglauer89@gmail.com>
2023-09-18 08:19:38 +0200

    Fix argument linking

--- chebai/cli.py
+++ chebai/cli.py
@@ -5,7 +5,7 @@ class ChebaiCLI(LightningCLI):
     def add_arguments_to_parser(self, parser):
         for kind in ("train", "val", "test"):
             for average in ("micro", "macro"):
-                parser.link_arguments("model.init_args.out_dim", f"model.init_args.{kind}_metrics.init_args.metrics.{average}-f1.init_args.num_labels", apply_on="instantiate")
+                parser.link_arguments("model.init_args.out_dim", f"model.init_args.{kind}_metrics.init_args.metrics.{average}-f1.init_args.num_labels")
 
 def cli():
     r = ChebaiCLI(save_config_callback=None, parser_kwargs={"parser_mode": "omegaconf"})
