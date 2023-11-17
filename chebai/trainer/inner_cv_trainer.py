9696cc1db1a20b74e256830d1093f6a2ccf73fa6
simon <simon.fluegel@ovgu.de>
2023-11-17 14:06:28 +0100

    fix trainer instantiation

--- chebai/trainer/inner_cv_trainer.py
+++ chebai/trainer/inner_cv_trainer.py
@@ -1,3 +1,4 @@
+import inspect
 from typing import Optional, Union
 import logging
 from lightning import Trainer, LightningDataModule, LightningModule
@@ -11,6 +12,8 @@ log = logging.getLogger(__name__)
 class InnerCVTrainer(Trainer):
 
     def __init__(self, *args, **kwargs):
+        self.init_args = args
+        self.init_kwargs = kwargs
         super().__init__(*args, **kwargs)
 
 
@@ -26,6 +29,7 @@ class InnerCVTrainer(Trainer):
             for fold, (train_ids, val_ids) in enumerate(kfold.split(datamodule.train_val_data)):
                 train_dataloader = datamodule.train_dataloader(ids=train_ids)
                 val_dataloader = datamodule.val_dataloader(ids=val_ids)
-                new_trainer = Trainer(*args, **kwargs)
+
+                new_trainer = Trainer(*self.init_args, **self.init_kwargs)
                 new_trainer.fit(train_dataloaders=train_dataloader, val_dataloaders=val_dataloader, *args, **kwargs)
 
