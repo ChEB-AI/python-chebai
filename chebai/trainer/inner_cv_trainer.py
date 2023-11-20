575382e4fad0cf85e352a16ded32757b323324a8
simon <simon.fluegel@ovgu.de>
2023-11-20 10:52:47 +0100

    debug log path

--- chebai/trainer/inner_cv_trainer.py
+++ chebai/trainer/inner_cv_trainer.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 from lightning import Trainer
+from lightning.pytorch.loggers import CSVLogger
 from sklearn import model_selection
 
 from chebai.preprocessing.datasets.base import XYBaseDataModule
@@ -30,8 +31,10 @@ class InnerCVTrainer(Trainer):
                 val_dataloader = datamodule.val_dataloader(ids=val_ids)
                 init_kwargs = self.init_kwargs
                 print(f'init_kwargs: {init_kwargs}')
-                init_kwargs['default_root_dir'] = os.path.join(self.default_root_dir, f'fold_{fold}')
-                new_trainer = Trainer(*self.init_args, **self.init_kwargs)
+                logger_cls = self.logger.__class__
+                new_logger = logger_cls(save_dir=os.path.join(self.default_root_dir, f'fold_{fold}'))
+                init_kwargs['logger'] = new_logger
+                new_trainer = Trainer(*self.init_args, init_kwargs)
                 print(f'new default_root_dir: {new_trainer.default_root_dir}')
                 print(f'new logger.save_dir: {new_trainer.logger.save_dir}')
                 new_trainer.fit(train_dataloaders=train_dataloader, val_dataloaders=val_dataloader, *args, **kwargs)
