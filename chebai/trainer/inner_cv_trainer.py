72a3c64d9c123dfc035291957980c75401538284
simon <simon.fluegel@ovgu.de>
2023-11-20 11:01:47 +0100

    fix logger save dir

--- chebai/trainer/inner_cv_trainer.py
+++ chebai/trainer/inner_cv_trainer.py
@@ -30,11 +30,9 @@ class InnerCVTrainer(Trainer):
                 train_dataloader = datamodule.train_dataloader(ids=train_ids)
                 val_dataloader = datamodule.val_dataloader(ids=val_ids)
                 init_kwargs = self.init_kwargs
-                print(f'init_kwargs: {init_kwargs}')
                 logger_cls = self.logger.__class__
-                new_logger = logger_cls(save_dir=os.path.join(self.default_root_dir, f'fold_{fold}'))
+                new_logger = logger_cls(save_dir=os.path.join(self.logger.save_dir, f'fold_{fold}'))
                 init_kwargs['logger'] = new_logger
                 new_trainer = Trainer(*self.init_args, **init_kwargs)
-                print(f'new default_root_dir: {new_trainer.default_root_dir}')
-                print(f'new logger.save_dir: {new_trainer.logger.save_dir}')
+                print(f'Using logger.save_dir: {new_trainer.logger.save_dir}')
                 new_trainer.fit(train_dataloaders=train_dataloader, val_dataloaders=val_dataloader, *args, **kwargs)
