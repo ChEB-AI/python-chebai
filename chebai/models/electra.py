950b6f0bfaca00adec91c0f9e6904d87035f9dc0
sfluegel <sfluegel@ovgu.de>
2023-11-22 16:47:41 +0100

    re-add ElectraPreLoss to model module to keep compatibility with pretrained model

--- chebai/models/electra.py
+++ chebai/models/electra.py
@@ -404,3 +404,18 @@ class ConeLoss:
             memberships, target.unsqueeze(-1).expand(-1, -1, 20)
         )
         return loss
+
+class ElectraPreLoss(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.ce = torch.nn.CrossEntropyLoss()
+
+    def forward(self, input, target, **loss_kwargs):
+        t, p = input
+        gen_pred, disc_pred = t
+        gen_tar, disc_tar = p
+        gen_loss = self.ce(target=torch.argmax(gen_tar.int(), dim=-1), input=gen_pred)
+        disc_loss = self.ce(
+            target=torch.argmax(disc_tar.int(), dim=-1), input=disc_pred
+        )
+        return gen_loss + disc_loss
