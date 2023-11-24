ad107a90900e8de6c741c06126de8aefbe67ed4b
sfluegel <sfluegel@ovgu.de>
2023-11-24 15:22:58 +0100

    fix conversion error in forward

--- chebai/models/electra.py
+++ chebai/models/electra.py
@@ -199,12 +199,11 @@ class Electra(ChebaiBaseNet):
     def forward(self, data, **kwargs):
         self.batch_size = data["features"].shape[0]
         try:
-            inp = self.electra.embeddings.forward(data["features"])
+            inp = self.electra.embeddings.forward(data["features"].int())
         except RuntimeError as e:
-            print(f'RuntimeError at forward')
-            print(f'data: {data}')
+            print(f'RuntimeError at forward: {e}')
             print(f'data[features]: {data["features"]}')
-            print(e)
+            raise Exception
         inp = self.word_dropout(inp)
         electra = self.electra(inputs_embeds=inp, **kwargs)
         d = electra.last_hidden_state[:, 0, :]
