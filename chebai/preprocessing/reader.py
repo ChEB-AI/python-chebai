8bf268a8e8e8d62d6b48f0a6653224ce1e99d6c7
sfluegel <sfluegel@ovgu.de>
2023-11-29 11:15:14 +0100

    fix selfies reader

--- chebai/preprocessing/reader.py
+++ chebai/preprocessing/reader.py
@@ -181,10 +181,10 @@ class SelfiesReader(ChemDataReader):
 
     def _read_data(self, raw_data):
         try:
-            tokenized = sf.split_selfies(sf.encoder(raw_data, strict=True))
+            tokenized = sf.split_selfies(sf.encoder(raw_data.strip(), strict=True))
             tokenized = [self._get_token_index(v) for v in tokenized]
         except Exception as e:
-            print(f'could not process {raw_data} (type: {type(raw_data)}')
+            print(f'could not process {raw_data}')
             print(f'\t{e}')
             self.error_count += 1
             print(f'\terror count: {self.error_count}')
