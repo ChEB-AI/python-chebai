3cce68649d7d1bf1d93e6977e37d8d1fe7ce4d85
aditya0by0 <aditya0by0@gmail.com>
2024-06-23 17:02:41 +0200

    added docstrings and type hints for callbacks

--- chebai/callbacks/prediction_callback.py
+++ chebai/callbacks/prediction_callback.py
@@ -1,16 +1,41 @@
+from lightning.pytorch import Trainer, LightningModule
 from lightning.pytorch.callbacks import BasePredictionWriter
 import torch
 import os
 import pickle
+from typing import Sequence, Any
 
 
 class PredictionWriter(BasePredictionWriter):
-    def __init__(self, output_dir, write_interval):
+    """
+    Custom callback for writing predictions to a file at the end of each epoch.
+
+    Args:
+        output_dir (str): The directory where prediction files will be saved.
+        write_interval (str): When to write predictions. Options are "batch" or "epoch".
+    """
+
+    def __init__(self, output_dir: str, write_interval: str):
         super().__init__(write_interval)
         self.output_dir = output_dir
         self.prediction_file_name = "predictions.pkl"
 
-    def write_on_epoch_end(self, trainer, pl_module, predictions, batch_indices):
+    def write_on_epoch_end(
+        self,
+        trainer: Trainer,
+        pl_module: LightningModule,
+        predictions: Sequence[Any],
+        batch_indices: Sequence[Any],
+    ) -> None:
+        """
+        Writes the predictions to a file at the end of the epoch.
+
+        Args:
+            trainer (Trainer): The Trainer instance.
+            pl_module (LightningModule): The LightningModule instance.
+            predictions (Sequence[Any]): Any sequence of predictions for the epoch.
+            batch_indices (Sequence[Any]): Any sequence of batch indices.
+        """
         results = [
             dict(
                 ident=row["data"]["idents"][0],
