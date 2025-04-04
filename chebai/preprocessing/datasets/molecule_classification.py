59064afafb7e259a4174e77780bf81e8e59d4d9f
schnamo <ch.tumescheit@gmail.com>
2025-04-04 16:29:01 +0200

    add HIV dataset handling

--- chebai/preprocessing/datasets/molecule_classification.py
+++ chebai/preprocessing/datasets/molecule_classification.py
@@ -41,7 +41,7 @@ class ClinTox(XYBaseDataModule):
     @property
     def raw_file_names(self) -> List[str]:
         """Returns a list of raw file names."""
-        return ["clintox.csv"]
+        return ["clintox_groups4.csv"]
 
     @property
     def processed_file_names(self) -> List[str]:
@@ -56,13 +56,13 @@ class ClinTox(XYBaseDataModule):
                 gout.name,
             )
             with gzip.open(gout.name) as gfile:
-                with open(os.path.join(self.raw_dir, "clintox.csv"), "wt") as fout:
+                with open(os.path.join(self.raw_dir, "clintox_groups4.csv"), "wt") as fout:
                     fout.write(gfile.read().decode())
 
     def setup_processed(self) -> None:
         """Processes and splits the dataset."""
         print("Create splits")
-        data = list(self._load_data_from_file(os.path.join(self.raw_dir, f"clintox.csv")))
+        data = list(self._load_data_from_file(os.path.join(self.raw_dir, f"clintox_groups4.csv")))
         groups = np.array([d["group"] for d in data])
         if not all(g is None for g in groups):
             split_size = int(len(set(groups)) * self.train_split)
@@ -85,12 +85,10 @@ class ClinTox(XYBaseDataModule):
             test_split = [
                 d
                 for d in (data[temp_split_index[i]] for i in test_split_index)
-                if d["original"]
             ]
             validation_split = [
                 d
                 for d in (data[temp_split_index[i]] for i in validation_split_index)
-                if d["original"]
             ]
         else:
             # print(self.train_split)
@@ -156,7 +154,9 @@ class ClinTox(XYBaseDataModule):
                 labels = [
                     bool(int(l)) if l else None for l in (row[k] for k in self.HEADERS)
                 ]
-                yield dict(features=smiles, labels=labels, ident=i)
+                group = int(row["group"])
+                yield dict(features=smiles, labels=labels, ident=i, group=group)
+                # yield dict(features=smiles, labels=labels, ident=i)
                 # yield self.reader.to_data(dict(features=smiles, labels=labels, ident=i))
 
 
@@ -532,6 +532,250 @@ class Bace(XYBaseDataModule):
                 yield dict(features=smiles, labels=labels, ident=i)
                 # yield self.reader.to_data(dict(features=smiles, labels=labels, ident=i))
 
+class HIV(XYBaseDataModule):
+    """Data module for ClinTox MoleculeNet dataset."""
+
+    HEADERS = [
+        "HIV_active",
+    ]
+
+    @property
+    def _name(self) -> str:
+        """Returns the name of the dataset."""
+        return "HIV"
+
+    @property
+    def label_number(self) -> int:
+        """Returns the number of labels."""
+        return 1
+
+    @property
+    def raw_file_names(self) -> List[str]:
+        """Returns a list of raw file names."""
+        return ["HIV.csv"]
+
+    @property
+    def processed_file_names(self) -> List[str]:
+        """Returns a list of processed file names."""
+        return ["test.pt", "train.pt", "validation.pt"]
+
+    def download(self) -> None:
+        
+        """Downloads and extracts the dataset."""
+        with open(os.path.join(self.raw_dir, "HIV.csv"), "ab") as dst:
+            with request.urlopen(f"https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/HIV.csv",) as src:
+                shutil.copyfileobj(src, dst)
+
+
+    def setup_processed(self) -> None:
+        """Processes and splits the dataset."""
+        print("Create splits")
+        data = list(self._load_data_from_file(os.path.join(self.raw_dir, f"HIV.csv")))
+        groups = np.array([d["group"] for d in data])
+        if not all(g is None for g in groups):
+            print('Group shuffled')
+            split_size = int(len(set(groups)) * self.train_split)
+            os.makedirs(self.processed_dir, exist_ok=True)
+            splitter = GroupShuffleSplit(train_size=split_size, n_splits=1)
+
+            train_split_index, temp_split_index = next(
+                splitter.split(data, groups=groups)
+            )
+
+            split_groups = groups[temp_split_index]
+
+            splitter = GroupShuffleSplit(
+                train_size=int(len(set(split_groups)) * self.train_split), n_splits=1
+            )
+            test_split_index, validation_split_index = next(
+                splitter.split(temp_split_index, groups=split_groups)
+            )
+            train_split = [data[i] for i in train_split_index]
+            test_split = [
+                d
+                for d in (data[temp_split_index[i]] for i in test_split_index)
+            ]
+            validation_split = [
+                d
+                for d in (data[temp_split_index[i]] for i in validation_split_index)
+            ]
+        else:
+            train_split, test_split = train_test_split(
+                    data, train_size=self.train_split, shuffle=True
+                )
+            test_split, validation_split = train_test_split(
+                    test_split, train_size=0.5, shuffle=True
+                )
+        for k, split in [
+            ("test", test_split),
+            ("train", train_split),
+            ("validation", validation_split),
+        ]:
+            print("transform", k)
+            torch.save(
+                split,
+                os.path.join(self.processed_dir, f"{k}.pt"),
+            )
+
+    def setup(self, **kwargs) -> None:
+        """Sets up the dataset by downloading and processing if necessary."""
+        if any(
+            not os.path.isfile(os.path.join(self.raw_dir, f))
+            for f in self.raw_file_names
+        ):
+            self.download()
+        if any(
+            not os.path.isfile(os.path.join(self.processed_dir, f))
+            for f in self.processed_file_names
+        ):
+            self.setup_processed()
+
+    def _load_dict(self, input_file_path: str) -> List[Dict]:
+        """Loads data from a CSV file.
+
+        Args:
+            input_file_path (str): Path to the CSV file.
+
+        Returns:
+            List[Dict]: List of data dictionaries.
+        """
+        i = 0
+        with open(input_file_path, "r") as input_file:
+            reader = csv.DictReader(input_file)
+            for row in reader:
+                i += 1
+                smiles = row["smiles"]
+                labels = [int(row["HIV_active"])]
+                # group = int(row["group"])
+                yield dict(features=smiles, labels=labels, ident=i) #, group=group)
+                # yield self.reader.to_data(dict(features=smiles, labels=labels, ident=i))
+
+
+class MUV(XYBaseDataModule):
+    """Data module for ClinTox MoleculeNet dataset."""
+
+    HEADERS = [
+        "MUV-466","MUV-548","MUV-600","MUV-644","MUV-652","MUV-689",
+        "MUV-692","MUV-712","MUV-713","MUV-733","MUV-737","MUV-810",
+        "MUV-832","MUV-846","MUV-852","MUV-858","MUV-859"
+    ]
+
+    @property
+    def _name(self) -> str:
+        """Returns the name of the dataset."""
+        return "MUV"
+
+    @property
+    def label_number(self) -> int:
+        """Returns the number of labels."""
+        return 17
+
+    @property
+    def raw_file_names(self) -> List[str]:
+        """Returns a list of raw file names."""
+        return ["muv.csv"]
+
+    @property
+    def processed_file_names(self) -> List[str]:
+        """Returns a list of processed file names."""
+        return ["test.pt", "train.pt", "validation.pt"]
+
+    def download(self) -> None:
+        """Downloads and extracts the dataset."""
+        with NamedTemporaryFile("rb") as gout:
+            request.urlretrieve(
+                "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/muv.csv.gz",
+                gout.name,
+            )
+            with gzip.open(gout.name) as gfile:
+                with open(os.path.join(self.raw_dir, "muv.csv"), "wt") as fout:
+                    fout.write(gfile.read().decode())
+
+    def setup_processed(self) -> None:
+        """Processes and splits the dataset."""
+        print("Create splits")
+        data = list(self._load_data_from_file(os.path.join(self.raw_dir, f"sider_groups4.csv")))
+        groups = np.array([d["group"] for d in data])
+        if not all(g is None for g in groups):
+            split_size = int(len(set(groups)) * self.train_split)
+            os.makedirs(self.processed_dir, exist_ok=True)
+            splitter = GroupShuffleSplit(train_size=split_size, n_splits=1)
+
+            train_split_index, temp_split_index = next(
+                splitter.split(data, groups=groups)
+            )
+
+            split_groups = groups[temp_split_index]
+
+            splitter = GroupShuffleSplit(
+                train_size=int(len(set(split_groups)) * self.train_split), n_splits=1
+            )
+            test_split_index, validation_split_index = next(
+                splitter.split(temp_split_index, groups=split_groups)
+            )
+            train_split = [data[i] for i in train_split_index]
+            test_split = [
+                d
+                for d in (data[temp_split_index[i]] for i in test_split_index)
+                # if d["original"]
+            ]
+            validation_split = [
+                d
+                for d in (data[temp_split_index[i]] for i in validation_split_index)
+                # if d["original"]
+            ]
+        else:
+            train_split, test_split = train_test_split(
+                data, train_size=self.train_split, shuffle=True
+            )
+            test_split, validation_split = train_test_split(
+                test_split, train_size=0.5, shuffle=True
+            )
+        for k, split in [
+            ("test", test_split),
+            ("train", train_split),
+            ("validation", validation_split),
+        ]:
+            print("transform", k)
+            torch.save(
+                split,
+                os.path.join(self.processed_dir, f"{k}.pt"),
+            )
+
+    def setup(self, **kwargs) -> None:
+        """Sets up the dataset by downloading and processing if necessary."""
+        if any(
+            not os.path.isfile(os.path.join(self.raw_dir, f))
+            for f in self.raw_file_names
+        ):
+            self.download()
+        if any(
+            not os.path.isfile(os.path.join(self.processed_dir, f))
+            for f in self.processed_file_names
+        ):
+            self.setup_processed()
+
+    def _load_dict(self, input_file_path: str) -> List[Dict]:
+        """Loads data from a CSV file.
+
+        Args:
+            input_file_path (str): Path to the CSV file.
+
+        Returns:
+            List[Dict]: List of data dictionaries.
+        """
+        i = 0
+        with open(input_file_path, "r") as input_file:
+            reader = csv.DictReader(input_file)
+            for row in reader:
+                i += 1
+                smiles = row["smiles"]
+                labels = [
+                    bool(int(l)) if l else None for l in (row[k] for k in self.HEADERS)
+                ]
+                # group = row["group"]
+                yield dict(features=smiles, labels=labels, ident=i)# , group=group)
+                # yield self.reader.to_data(dict(features=smiles, labels=labels, ident=i))
 
 class BaceChem(Bace):
     """Chemical data reader for Tox21MolNet dataset."""
@@ -551,6 +795,12 @@ class BBBPChem(BBBP):
 
 
 class ClinToxChem(ClinTox):
+    """Chemical data reader for Tox21MolNet dataset."""
+
+    READER = dr.ChemDataReader
+
+
+class HIVChem(HIV):
     """Chemical data reader for Tox21MolNet dataset."""
 
     READER = dr.ChemDataReader
