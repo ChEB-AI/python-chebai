0a62a95bba5cd8d128998f0265cb42b5a81cab81
sfluegel <simon.fluegel@uni-osnabrueck.de>
2025-04-07 11:08:33 +0200

    reformat using black

--- chebai/preprocessing/datasets/base.py
+++ chebai/preprocessing/datasets/base.py
@@ -314,8 +314,13 @@ class XYBaseDataModule(LightningDataModule):
             if d["features"] is not None
         ]
         # filter for missing features in resulting data, keep features length below token limit
-        data = [val for val in data if val["features"] is not None
-                and self.n_token_limit is None or len(val["features"]) <= self.n_token_limit]
+        data = [
+            val
+            for val in data
+            if val["features"] is not None
+            and self.n_token_limit is None
+            or len(val["features"]) <= self.n_token_limit
+        ]
 
         return data
 
