0e7d55bd03a98b6e2e4cb4559475512ed38a2927
Simon Flügel <43573433+sfluegel05@users.noreply.github.com>
2025-04-24 14:32:46 +0200

    Update setup.py
    update version number and numpy version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ packages = find_packages()
 print(packages)
 setup(
     name="chebai",
-    version="0.0.2.dev0",
+    version="1.0.0",
     packages=packages,
     package_data={"": ["**/*.txt", "**/*.json"]},
     include_package_data=True,
@@ -20,7 +20,7 @@ setup(
         "idna",
         "joblib",
         "networkx",
-        "numpy<2",
+        "numpy",
         "pandas",
         "python-dateutil",
         "pytz",
