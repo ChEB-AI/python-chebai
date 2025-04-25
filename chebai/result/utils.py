32ea3221549f93fdc5b9014ddcb1af7f175c236b
sfluegel <simon.fluegel@uni-osnabrueck.de>
2025-04-25 18:11:53 +0200

    fix: model eval ignored the last samples of the test set

--- chebai/result/utils.py
+++ chebai/result/utils.py
@@ -118,6 +118,7 @@ def evaluate_model(
     else:
         data_list = data_module.load_processed_data("test", filename)
     data_list = data_list[: data_module.data_limit]
+    print(f"Loaded {len(data_list)} data points for evaluation.")
     preds_list = []
     labels_list = []
     if buffer_dir is not None:
@@ -160,7 +161,7 @@ def evaluate_model(
             test_labels = _concat_tuple(labels_list)
             return test_preds, test_labels
         return test_preds, None
-    elif len(preds_list) < 0:
+    elif len(preds_list) > 0:
         if len(preds_list) > 0 and preds_list[0] is not None:
             torch.save(
                 _concat_tuple(preds_list),
