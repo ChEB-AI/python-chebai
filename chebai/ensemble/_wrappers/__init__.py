41dd1c6c832e83d12d2792b8c667a4ecd81d821a
sfluegel <simon.fluegel@uni-osnabrueck.de>
2025-06-24 14:46:24 +0200

    move ensemble to chebifier repo, move property calculation and utils to results

--- chebai/ensemble/_wrappers/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._base import BaseWrapper
-from ._chemlog import ChemLogWrapper
-from ._gnn import GNNWrapper
-from ._neural_network import NNWrapper
-
-__all__ = ["NNWrapper", "BaseWrapper", "GNNWrapper", "ChemLogWrapper"]
