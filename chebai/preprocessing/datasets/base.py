06c09043344a708956834978ed954ed92649e1f1
aditya0by0 <aditya0by0@gmail.com>
2025-08-22 14:56:57 +0200

    separate splits file for aug data

--- chebai/preprocessing/datasets/base.py
+++ chebai/preprocessing/datasets/base.py
@@ -856,7 +856,7 @@ class _DynamicDataset(XYBaseDataModule, ABC):
         pass
 
     @abstractmethod
-    def select_classes(self, g: nx.DiGraph, *args, **kwargs) -> List:
+    def select_classes(self, g: nx.DiGraph, *args, **kwargs) -> list:
         """
         Selects classes from the dataset based on a specified criteria.
 
@@ -880,7 +880,7 @@ class _DynamicDataset(XYBaseDataModule, ABC):
         """
         pd.to_pickle(data, open(os.path.join(self.processed_dir_main, filename), "wb"))
 
-    def get_processed_pickled_df_file(self, filename: str) -> pd.DataFrame | None:
+    def get_processed_pickled_df_file(self, filename: str) -> Optional[pd.DataFrame]:
         """
         Gets the processed dataset pickle file.
 
@@ -1004,7 +1004,8 @@ class _DynamicDataset(XYBaseDataModule, ABC):
 
         combined_split_assignment = pd.concat(split_assignment_list, ignore_index=True)
         combined_split_assignment.to_csv(
-            os.path.join(self.processed_dir_main, "splits.csv"), index=False
+            os.path.join(self.processed_dir_main, self.splits_file_name),
+            index=False,
         )
 
         # Store the splits in class variables
@@ -1264,3 +1265,13 @@ class _DynamicDataset(XYBaseDataModule, ABC):
         if self.n_token_limit is not None:
             return {"data": f"data_maxlen{self.n_token_limit}.pt"}
         return {"data": "data.pt"}
+
+    @property
+    def splits_file_name(self) -> str:
+        """
+        Returns the name of the splits file.
+
+        Returns:
+            str: The name of the splits file.
+        """
+        return "splits.csv"
