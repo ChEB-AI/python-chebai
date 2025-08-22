03d5e55d127cf4d4a71274511c0c770bc8542ec6
sfluegel <simon.fluegel@uni-osnabrueck.de>
2025-08-22 14:08:23 +0200

    fix lstm

--- chebai/models/lstm.py
+++ chebai/models/lstm.py
@@ -9,21 +9,23 @@ logging.getLogger("pysmiles").setLevel(logging.CRITICAL)
 
 
 class ChemLSTM(ChebaiBaseNet):
-    def __init__(self, out_d, in_d, num_classes, criterion : nn.Module=None, **kwargs):
+    def __init__(self, out_d, in_d, num_classes, criterion: nn.Module = None, **kwargs):
         super().__init__(
             out_dim=out_d,
             input_dim=in_d,
             criterion=criterion,
             num_classes=num_classes,
-            **kwargs
+            **kwargs,
         )
-        self.lstm = nn.LSTM(in_d, out_d, batch_first=True, dropout=0.2, bidirectional=True)
+        self.lstm = nn.LSTM(
+            in_d, out_d, batch_first=True, dropout=0.2, bidirectional=True
+        )
         self.embedding = nn.Embedding(1400, in_d)
         self.output = nn.Sequential(
-            nn.Linear(out_d * 2, in_d),
+            nn.Linear(out_d, out_d),
             nn.ReLU(),
             nn.Dropout(0.2),
-            nn.Linear(in_d, num_classes),
+            nn.Linear(out_d, num_classes),
         )
 
     def forward(self, data, *args, **kwargs):
