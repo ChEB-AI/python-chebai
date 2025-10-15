1c5f5bcf66bf807624753f5ffc9e8ded0d66a6cd
jcapp4 <jcappellani@uni-osnabrueck.de>
2025-10-16 01:48:50 +0200

    small fix

--- chebai/models/base.py
+++ chebai/models/base.py
@@ -269,10 +269,10 @@ class ChebaiBaseNet(LightningModule, ABC):
                     model_output, labels, data.get("loss_kwargs", dict())
                 )
                 loss_kwargs = dict()
-                loss_kwargs['weights'] = f.create_data_weights(batchsize=len(data['idents']),dim=data['labels'].size(dim=1),weights=data["loss_kwargs"],idents=data["idents"])
                 if self.pass_loss_kwargs:
                     loss_kwargs = loss_kwargs_candidates
                 #torch.save(loss_data,"loss_data.pt")
+                loss_kwargs['weights'] = f.create_data_weights(batchsize=len(data['idents']),dim=data['labels'].size(dim=1),weights=data["loss_kwargs"],idents=data["idents"])
                 loss_kwargs["current_epoch"] = self.trainer.current_epoch
                 loss = self.criterion(loss_data, loss_labels, **loss_kwargs)
                 if isinstance(loss, tuple):
