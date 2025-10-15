b6b668be62fccac0090d6b3704b4be6d41fe8a78
jcapp4 <jcappellani@uni-osnabrueck.de>
2025-10-15 18:59:05 +0200

    had loading weights in debuggmode

--- chebai/preprocessing/datasets/base.py
+++ chebai/preprocessing/datasets/base.py
@@ -1185,7 +1185,7 @@ class _DynamicDataset(XYBaseDataModule, ABC):
                 data = data_df.to_dict(orient="records")
                 if kind == "train":
                     f.init_weights()
-                    data = f.add_val_weights(data)
+                    data = f.add_train_weights(data)
                 if kind == "validation":
                     data = f.add_val_weights(data)
                 # torch.save(data,"gewicht.pt")
