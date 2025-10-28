ff0727942a4b03cac8be41e079ec26ba2234634f
jcapp4 <jcappellani@uni-osnabrueck.de>
2025-10-28 16:42:55 +0100

    fix for evaluation issue

--- chebai/result/utils.py
+++ chebai/result/utils.py
@@ -157,7 +157,7 @@ def evaluate_model(
             test_labels = _concat_tuple(labels_list)
             return test_preds, test_labels
         return test_preds, None
-    elif len(preds_list) < 0:
+    elif len(preds_list) > 0:
         if len(preds_list) > 0 and preds_list[0] is not None:
             torch.save(
                 _concat_tuple(preds_list),
