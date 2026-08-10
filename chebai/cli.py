2f18c58bb219d8d8867aa64aac5f5959956a2aea
aditya0b0 <aditya0by0@gmail.com>
2026-08-10 19:57:10 +0200

    fix pr-auc link error

--- chebai/cli.py
+++ chebai/cli.py
@@ -75,6 +75,7 @@ class ChebaiCLI(LightningCLI):
                 "mse",
                 "rmse",
                 "r2",
+                "pr-auc",
             ):
                 # When using lightning > 2.5.1 then need to uncomment all metrics that are not used
                 # for average in ("mse", "rmse","r2"): # for regression
